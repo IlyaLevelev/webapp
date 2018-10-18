a4cc21b04b881af0e79528dddc93974c34a3c8f5
IlyaLevelev <ilja.levelev@gmail.com>
2018-10-18 15:55:22 +0200

    Included webapp exercise

--- src/main/java/io/github/ilyalevelev/webapp/SparkMain.java
+++ src/main/java/io/github/ilyalevelev/webapp/SparkMain.java
@@ -8,11 +8,21 @@ public class SparkMain {
 
         Spark.get("/about", (req, res) -> "about the company");
 
+        // When you launch the server, you can access this endpoint with http://localhost:4567/news
         Spark.get("/news", (req, res) -> {
+
+            // Define a News class
+            // Every News has title and text
+
+            // Here create 3 News objects and put them in a list
+            // Loop through the list and built the html
 
             String html = "";
             html = html + "<h1>The news</h1>\n";
-            html = html + "<p>the latest news will appear here<p>";
+
+            // You will remove this part, because you will add the actual news
+            html = html + "<h3>Hurricane in Texas</h3>\n";
+            html = html + "<p>There was bla bla...<p>";
 
             System.out.println(html);
 
