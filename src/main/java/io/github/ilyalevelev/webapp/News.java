a0162edb06ee807906e98be7b66b0e5fbccd0a6b
IlyaLevelev <ilja.levelev@gmail.com>
2018-10-21 19:39:06 +0200

    Created News class

---
+++ src/main/java/io/github/ilyalevelev/webapp/News.java
@@ -0,0 +1,15 @@
+package io.github.ilyalevelev.webapp;
+
+public class News {
+
+    string title;
+    string text;
+
+    Public News (string title, string text){
+
+        this.title = title;
+        this.text = text;
+    }
+
+
+}
--- src/main/java/io/github/ilyalevelev/webapp/SparkMain.java
+++ src/main/java/io/github/ilyalevelev/webapp/SparkMain.java
@@ -1,6 +1,12 @@
 package io.github.ilyalevelev.webapp;
 
+import javafx.scene.input.InputMethodTextRun;
 import spark.Spark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.awt.SystemColor.text;
 
 public class SparkMain {
 
@@ -16,6 +22,22 @@ public class SparkMain {
 
             // Here create 3 News objects and put them in a list
             // Loop through the list and built the html
+
+            News news1 = new News(title "Badalona opens new Port", text "Today Badalona opened it new port that will connect the city with the sea")
+            News news2 = new News(title "Microsoft acquires GitHub", text "The big Software giant says connecting with IT engineers becomes increasingly important")
+            News news3 = new News(title "New James Bond movie arrives in Cinema", text "Daniel Craig said it will be the last time he stars in a James Bond movie")
+
+            List<News> News = new ArrayList<>();
+            News.add(news1);
+            News.add(news2);
+            News.add(news3);
+
+            for (News N : News) {
+                String html = "";
+                html = html + "<h1>The news</h1>\n"
+
+
+            }
 
             String html = "";
             html = html + "<h1>The news</h1>\n";
